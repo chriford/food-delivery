900954322120127b2bc181542f0d2ecc556419a7
chriford <siamechrif@gmail.com>
2022-12-13 15:41:39 +0200

    create the cart screen

---
+++ src/screens/Cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const CartScreen = ({navigation, route}) => {
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <StatusBar translucent backgroundColor='rgba(0,0,0,0)' />
+            
+        </SafeAreaView>
+    )
+}
+export default CartScreen;
